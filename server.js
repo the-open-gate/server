8414c3f5bd06719d4c36d0b9091a896f5ee0fa33
the-open-gate <84135508+the-open-gate@users.noreply.github.com>
2021-05-13 19:18:27 +0530

    added port variable

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(5000, () => console.log(`listining on port 5000`));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`listining on port ${PORT}`));
 
 app.get("/", (req, res) => {
   res.status(200).send("The Open Gate Server Engaged");
